35b32c37ea07a54c96419e6698c7e88f4c5f570c
Ulf Zackrisson <ulmezz@gmail.com>
2018-04-25 20:30:23 +0200

    Fix vertical align middle in CompanyListing.js file

--- src/users/companyListing/CompanyListing.js
+++ src/users/companyListing/CompanyListing.js
@@ -10,8 +10,8 @@ class CompanyListing extends React.Component {
     render() {
         return (
             <div className="row mt-5 mb-3 border border-secondary company-listing-main">
-                <div className="col-3 company-listing-padding"><a href="http://placeholder.com"><img src="http://via.placeholder.com/50x50" alt="test" /></a></div>
-                <div className="col-3 company-listing-padding">
+                <div className="col-3 my-auto company-listing-padding"><a href="http://placeholder.com"><img src="http://via.placeholder.com/50x50" alt="test" /></a></div>
+                <div className="col-3 my-auto company-listing-padding">
                     <div className="row">
                         <div className="col-12">
                             <span className="company-listing-link" onClick={this.onModalButtonClick}>Key Account Manager</span>
@@ -21,13 +21,13 @@ class CompanyListing extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="col-3 company-listing-padding company-listing-city"><i className="fa fa-map-marker-alt company-listing-icon"></i>&nbsp;Stockholm</div>
-                <div className="col-3 company-listing-padding company-listing-intern-positions">
+                <div className="col-3 my-auto company-listing-padding company-listing-city"><i className="fa fa-map-marker-alt company-listing-icon"></i>&nbsp;Stockholm</div>
+                <div className="col-3 my-auto company-listing-padding company-listing-intern-positions">
                     <div className="row">
                         <div className="col-12 text-center company-listing-bold">
                             Praktikplats
                         </div>
-                        <div className="col-12 text-center">
+                        <div className="col-12 mt-2 text-center">
                             Upplagt 2 månader sen
                         </div>
                         <div className="col-12 text-center">
