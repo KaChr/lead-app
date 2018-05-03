e4894f7286a442997ef4a5f9eb9d3c6cad0ceaf7
jonon <jonathancjonsson@outlook.com>
2018-05-03 11:48:25 +0200

    Fix layout

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Logga In</h1>
-                <form>
-                    <input type="text" placeholder="Email"/>
-                    <input type="password" placeholder="Lösenord" />
-                </form>
+            <div className="bg d-flex align-items-center">
+                <div className="container container-small bg-transparent-black">
+                    <div className="row justify-content-center">
+                    <div className="col-10">
+                    <h1 className="text-center">Logga In</h1>
+                    <form className="login-form">
+                        <input type="text" className=" input-form form-control" placeholder="Email"/>
+                        <input type="password" className="input-form form-control" placeholder="Lösenord" />
+                    </form>
+                    <div>
+                        <p>Har du inget konto? Registrera dig <a href="register.html">Här</a>.</p>
+                    </div>
+
+                        <a href="#"><button className="btn btn-danger">Logga In</button></a>
+
+                        <a className="cancel-register" href="index.html">Avbryt</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
