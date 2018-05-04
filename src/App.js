6e5717369cb24ddf77564dacee69c588fb3eeedb
Michael Tariao <michael.tariao@chasacademy.se>
2018-05-04 13:02:02 +0200

    removed comments

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      //   <button type="button" className="btn btn-primary">Primary</button>
-      // </div>
-
       <div className="landing-page">
         <p>Place header here</p>
         <div className="jumbo-tron">
