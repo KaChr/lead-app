ea4efda38e90278044bab7e96a65f797d5cf472b
jonon <jonathancjonsson@outlook.com>
2018-05-11 14:37:29 +0200

    Add bootstrap class to fix description layout

--- src/pages/companyProfile/CompanyProfile.js
+++ src/pages/companyProfile/CompanyProfile.js
@@ -8,7 +8,7 @@ import CompanyProfileDescription from './CompanyProfileDescription';
 class CompanyProfile extends React.Component {
     render() {
         return(
-            <div>
+            <div className="companyProfile__container--height">
                 <div className="companyProfile__banner"></div>
                 <div className="container shadow company-profile__container">
                     <div className="row border-bottom">
@@ -18,14 +18,16 @@ class CompanyProfile extends React.Component {
                         </div> */}
                         <div className="container" >
                             <div className="row">
-                                <div className="companyProfile__logo--2 col-5 "></div>
-                                <ul className="col">
-                                    <li>My Company</li>
-                                    <li>Stad: </li>
-                                    <li>Adress: </li>
-                                    <li>E-mail: </li>
-                                    <li>Hemsida: </li>
-                                </ul>  
+                                <div className="companyProfile__logo--2 col-5"></div>
+                                    <div className="d-flex align-items-center">
+                                        <ul className="col list-unstyled mr-8">
+                                            <li>My Company</li>
+                                            <li>Stad: </li>
+                                            <li>Adress: </li>
+                                            <li>E-mail: </li>
+                                            <li>Hemsida: </li>
+                                        </ul>
+                                </div>  
                             </div>
                         </div>                
                     </div>
