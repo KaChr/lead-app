e67d6acf95454e015153a0569ac5d628fb04f540
jonon <jonathancjonsson@outlook.com>
2018-05-14 11:35:40 +0200

    change logo background-image to img tag

--- src/pages/companyProfile/CompanyProfile.js
+++ src/pages/companyProfile/CompanyProfile.js
@@ -9,25 +9,28 @@ class CompanyProfile extends React.Component {
     render() {
         return(
             <div className="companyProfile__container--height">
-                <div className="companyProfile__banner"></div>
-                <div className="container shadow company-profile__container ">
+                <div className="companyProfile__banner">
+                </div>
+                <div className="container shadow company-profile__container pb-3">
                     <div className="row border-bottom">
-                        {/* <div className="col-4">
-                            <img className
-                            ="companyProfile__logo" src={companyExampleLogo}/>
-                        </div> */}
+                        
                         <div className="container" >
                             <div className="row">
-                                <div className="companyProfile__logo--2 col-5"></div>
-                                    <div className="d-flex align-items-center">
-                                        <ul className="col list-unstyled mr-8">
-                                            <li className="font-weight-bold">My Company</li>
-                                            <li>Stad: </li>
-                                            <li>Adress: </li>
-                                            <li>E-mail: </li>
-                                            <li>Hemsida: </li>
-                                        </ul>
-                                    </div>  
+                                <div className="companyProfile__logo--2 col-5">
+                                <div className="">
+                                    <img className
+                                    ="companyProfile__logo" src={companyExampleLogo}/>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center">
+                                    <ul className="col list-unstyled mr-8">
+                                        <li className="font-weight-bold">My Company</li>
+                                        <li>Stad: </li>
+                                        <li>Adress: </li>
+                                        <li>E-mail: </li>
+                                        <li>Hemsida: </li>
+                                    </ul>
+                                </div>  
                             </div>
                         </div>                
                     </div>
