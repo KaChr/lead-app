1c5208c653bd758f233c3d8a8f6357326bec7eb2
jonon <jonathancjonsson@outlook.com>
2018-05-23 16:37:03 +0200

    rename file structure in .js file

---
+++ src/pages/companyLoggedInFreePremium/companyLoggedInFreePremium.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './companyLoggedInFreePremium.css';
+
+class companyLoggedInFreePremium extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default companyLoggedInFreePremium;
