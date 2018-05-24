223909382f772bfa34782cace2dd04e4f2ed1c18
jonon <jonathancjonsson@outlook.com>
2018-05-24 11:00:07 +0200

    add banner and button

--- src/pages/companyLoggedInFree/companyLoggedInFree.js
+++ src/pages/companyLoggedInFree/companyLoggedInFree.js
@@ -5,7 +5,8 @@ class companyLoggedInFree extends React.Component {
     render() {
         return(
             <div>
-                
+                <div className="companyLoggedInFreeBanner"></div>
+                <button className="btn btn-danger">Lägg till annons</button>
             </div>
         );
     }
