3e7fbc05f7d9045d421f928a37be210ed77b2132
jonon <jonathancjonsson@outlook.com>
2018-05-24 14:32:56 +0200

    made small changes with axios

--- src/pages/companyLoggedInFree/companyLoggedInFree.js
+++ src/pages/companyLoggedInFree/companyLoggedInFree.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import './companyLoggedInFree.css';
+
 
 import Button from '../../Components/Button/Button';
 import '../../Components/Button/Button.css';
@@ -8,26 +10,54 @@ import { Authorization } from '../../Components/Helper/Authorization';
 
 
 class companyLoggedInFree extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            companyListing : []
+        }
+
+    }
 
     componentDidMount() {
         Authorization(this);
+
+
+        const token = localStorage.getItem('token');
+        console.log(token);
+
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/verify-token`, null, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+
+            .then((res) => {
+                console.log(res.data);
+                //axios.get(`${process.env.REACT_APP_API_BASE_URL}/company-listings/${res.data.userId}`)
+
+            })
+
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     render() {
-        return(
+        return (
             <div>
                 <div className="row">
                     <div className="col-12 companyLoggedInFreeBanner"></div>
                     <div className="justify-content-center">
-                    <Button url="/company_listing_edit" name="Lägg till annonser"/>
-                    </div>  
+                        <Button url="/company_listing_edit" name="Lägg till annonser" />
+                    </div>
                 </div>
                 <div className="">
                     <p>Dina annonser:</p>
                 </div>
-        </div>
+            </div>
         );
     }
 }
 
-export default companyLoggedInFree;
+    export default companyLoggedInFree;
